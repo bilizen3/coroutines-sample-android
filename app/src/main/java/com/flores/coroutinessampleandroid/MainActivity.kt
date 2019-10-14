d9ec2de63711d55e049738f125365fa861e2c55d
Bill Flores <bilizen3@gmail.com>
2019-10-13 20:23:01 -0500

    add comment to MainActivity

--- app/src/main/java/com/flores/coroutinessampleandroid/MainActivity.kt
+++ app/src/main/java/com/flores/coroutinessampleandroid/MainActivity.kt
@@ -3,6 +3,12 @@ package com.flores.coroutinessampleandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * MainActivity
+ *
+ * @author Bill Flores - bilizen3@gmail.com
+ * @since 10/13/2019
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
