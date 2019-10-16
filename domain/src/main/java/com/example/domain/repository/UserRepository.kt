c66037e42cc5a3ba16cd049c6d74e974f8ef2c9f
bill.flores <bilizen3@gmail.com>
2019-10-16 17:08:17 -0500

    add user repository

---
+++ domain/src/main/java/com/example/domain/repository/UserRepository.kt
@@ -0,0 +1,16 @@
+package com.example.domain.repository
+
+/**
+ * UserRepository
+ *
+ * @author Bill Flores - bilizen3@gmail.com
+ * @since 16/10/2019
+ */
+interface UserRepository {
+
+    fun insertUser()
+
+    fun deleteUser()
+
+    fun getUser()
+}
