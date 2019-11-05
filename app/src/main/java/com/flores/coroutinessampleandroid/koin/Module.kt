50cb2d3692971e807f3b25ef8298e69f597c46e4
bill.flores <bilizen3@gmail.com>
2019-11-05 14:46:52 -0500

    add Module class

---
+++ app/src/main/java/com/flores/coroutinessampleandroid/koin/Module.kt
@@ -0,0 +1,28 @@
+package com.flores.coroutinessampleandroid.koin
+
+import com.flores.coroutinessampleandroid.MainViewModel
+import com.flores.coroutinessampleandroid.db.AppDataBase
+import com.flores.coroutinessampleandroid.repository.UserRepository
+import com.flores.coroutinessampleandroid.repository.UserRepositoryImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/**
+ * Module
+ *
+ * @author Bill Flores - bilizen3@gmail.com
+ * @since 05/11/2019
+ */
+
+val appModule = module {
+
+    factory {
+        AppDataBase.getInstance(androidContext())
+    }
+
+    single<UserRepository> { UserRepositoryImpl(get()) }
+
+    viewModel { MainViewModel(get()) }
+
+}
